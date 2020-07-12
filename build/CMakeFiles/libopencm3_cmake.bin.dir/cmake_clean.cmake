6d869b8b1d2a6859ec3dc9ae6df910a10e143614
Nam Tran <nhtranngoc@wpi.edu>
2020-07-11 20:37:27 -0400

    Oops, forgot to ignore build folder. Fixed

--- build/CMakeFiles/libopencm3_cmake.bin.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/libopencm3_cmake.bin"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/libopencm3_cmake.bin.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- build/CMakeFiles/yarc.bin.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/yarc.bin"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/yarc.bin.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
